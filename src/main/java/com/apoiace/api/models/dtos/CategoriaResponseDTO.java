9175cb77de3bdb4b1419bffe68f2a32de046e2b1
EmsRibeiro <jrc.emerson@gmail.com>
2026-04-09 17:17:48 -0300

    feat: add CategoriaRespondeDTO.java at models/dtos

--- src/main/java/com/apoiace/api/models/dtos/CategoriaResponseDTO.java
+++ src/main/java/com/apoiace/api/models/dtos/CategoriaResponseDTO.java
@@ -2,3 +2,21 @@ package com.apoiace.api.models.dtos;
 
 public class CategoriaResponseDTO{
 }
+@Getter
+@Setter
+public class CategoriaResponseDTO {
+
+    private Long id;
+    private String nome;
+    private String cor;
+    private OffsetDateTime criadoEm;
+
+    public static CategoriaResponseDTO fromEntity(Categoria categoria) {
+        CategoriaResponseDTO dto = new CategoriaResponseDTO();
+        dto.setId(categoria.getId());
+        dto.setNome(categoria.getNome());
+        dto.setCor(categoria.getCor());
+        dto.setCriadoEm(categoria.getCriadoEm());
+        return dto;
+    }
+}
