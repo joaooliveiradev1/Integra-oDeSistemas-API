a7da033fd902fcebe79250a336c8dd16382ad839
EmsRibeiro <jrc.emerson@gmail.com>
2026-04-09 17:16:56 -0300

    feat: add CategoriaRequestDTO.java at models/dtos

--- src/main/java/com/apoiace/api/models/dtos/CategoriaRequestDTO.java
+++ src/main/java/com/apoiace/api/models/dtos/CategoriaRequestDTO.java
@@ -2,3 +2,14 @@ package com.apoiace.api.models.dtos;
 
 public class CategoriaRequestDTO {
 }
+@Getter
+@Setter
+public class CategoriaRequestDTO {
+
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(max = 80, message = "Nome deve ter no máximo 80 caracteres")
+    private String nome;
+
+    @Pattern(regexp = "^#([A-Fa-f0-9]{6})$", message = "Cor deve ser um hex válido ex: #3B82F6")
+    private String cor;
+}
