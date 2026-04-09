23713b9a579404d0c1369e6c7a0b5988bf61f78e
EmsRibeiro <jrc.emerson@gmail.com>
2026-04-09 17:12:51 -0300

    feat: add CategoriaRepository.java

--- src/main/java/com/apoiace/api/repository/CategoriaRepository.java
+++ src/main/java/com/apoiace/api/repository/CategoriaRepository.java
@@ -2,3 +2,17 @@ package com.apoiace.api.repository;
 
 public interface CategoriaRepository {
 }
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+    List<Categoria> findAllByDeletedAtIsNull();
+
+    Optional<Categoria> findByIdAndDeletedAtIsNull(Long id);
+
+    boolean existsByNomeIgnoreCaseAndDeletedAtIsNull(String nome);
+
+    boolean existsByNomeIgnoreCaseAndDeletedAtIsNullAndIdNot(String nome, Long id);
+
+    @Query("SELECT c FROM Categoria c WHERE LOWER(c.nome) LIKE LOWER(CONCAT('%', :nome, '%')) AND c.deletedAt IS NULL")
+    List<Categoria> findByNomeContainingIgnoreCase(String nome);
+}
